382959b74c1766cc4969ed5b95a3327145c715b5
Eman Basic <basic.eman@hotmail.com>
2024-09-08 15:19:15 +0200

    create LocationRadiusPickerController

---
+++ Sources/LocationRadiusPicker/LocationRadiusPickerController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationRadiusPickerController.swift
+//
+//
+//  Created by Eman Basic on 08.09.24.
+//
+
+import UIKit
+
+final class LocationRadiusPickerController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
