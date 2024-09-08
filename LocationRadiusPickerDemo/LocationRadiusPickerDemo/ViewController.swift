e8d5265a313fe8035b8f530cc1d82e53f4dc2651
Eman Basic <basic.eman@hotmail.com>
2024-09-08 15:32:40 +0200

    create demo project

---
+++ LocationRadiusPickerDemo/LocationRadiusPickerDemo/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  LocationRadiusPickerDemo
+//
+//  Created by Eman Basic on 08.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Views
+    
+    private lazy var openPickerButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.background.backgroundColor = .systemBlue
+        config.title = "Open picker"
+        
+        let view = UIButton(configuration: config)
+        view.addAction(UIAction { [unowned self] _ in
+            onOpenPickerButtonPressed()
+        }, for: .touchUpInside)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        setup()
+    }
+}
+
+// MARK: - Setup
+
+extension ViewController {
+    private func setup() {
+        view.addSubview(openPickerButton)
+        
+        NSLayoutConstraint.activate([
+            openPickerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            openPickerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+// MARK: - Action
+
+extension ViewController {
+    private func onOpenPickerButtonPressed() {
+        
+    }
+}
