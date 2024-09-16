b252bcd4f307f4e9a470b442c39341112afac766
Eman Basic <basic.eman@hotmail.com>
2024-09-16 13:44:11 +0200

    improve the documentation of the LocationRadiusPickerConfigurationBuilder

--- Sources/LocationRadiusPicker/LocationRadiusPickerConfigurationBuilder.swift
+++ Sources/LocationRadiusPicker/LocationRadiusPickerConfigurationBuilder.swift
@@ -14,104 +14,130 @@ public class LocationRadiusPickerConfigurationBuilder {
     
     /// Creates the LocationRadiusPickerConfigurationBuilder with default radius parameters.
     /// - Parameters:
-    ///   - initialRadius: the initial radius the circle will be set to
-    ///   - minimumRadius: minimum allowed radius the circle can shrink to
-    ///   - maximumRadius: maximum allowed radius the circle can expand to
+    ///   - initialRadius: the initial radius (in meters) the circle will be set to
+    ///   - minimumRadius: minimum allowed radius (in meters) the circle can shrink to
+    ///   - maximumRadius: maximum allowed radius (in meters) the circle can expand to
+    ///
+    /// - Note: If the initial radius is outside the range [min, max], the value will be capped to fit that interval.
     public init(initialRadius: Double, minimumRadius: Double, maximumRadius: Double) {
         configuration = LocationRadiusPickerConfiguration(radius: initialRadius, minimumRadius: minimumRadius, maximumRadius: maximumRadius)
     }
     
-    /// The title of the picker, shown on the navigation bar. Defaults to "Location Radius Picker".
+    /// The title of the picker, shown on the navigation bar.
     /// - Parameter title: title of the picker
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: **"Location Radius Picker"** (not localised).
     public func title(_ title: String) -> LocationRadiusPickerConfigurationBuilder {
         configuration.title = title
         return self
     }
     
-    /// The title of the save button, located on the navigation bar. Defaults to "Save".
+    /// The title of the save button, located on the navigation bar.
     /// - Parameter title: title of the save button
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: **"Save"** (not localised).
     public func saveButtonTitle(_ title: String) -> LocationRadiusPickerConfigurationBuilder {
         configuration.saveButtonTitle = title
         return self
     }
     
-    /// The title of the cancel button, located on the navigation bar. Defaults to "Cancel".
+    /// The title of the cancel button, located on the navigation bar.
     /// - Parameter title: title of the cancel button
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: **"Cancel"** (not localised).
     public func cancelButtonTitle(_ title: String) -> LocationRadiusPickerConfigurationBuilder {
         configuration.cancelButtonTitle = title
         return self
     }
     
-    /// Initial location where the circle will be placed on the map. Defaults to Apple Inc. - Infinite Loop Campus.
+    /// Initial location where the circle will be placed on the map.
     /// - Parameter location: location coordinates of the circle
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: **Apple Inc. - Infinite Loop Campus** (Lat: 37.331711, Lon: -122.030773)
     public func initialLocation(_ location: CLLocationCoordinate2D) -> LocationRadiusPickerConfigurationBuilder {
         configuration.location = location
         return self
     }
     
-    /// Color of the radius circle border. Defaults to UIColor.systemBlue.
+    /// Color of the radius circle border.
     /// - Parameter color: color of the radius circle border
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `UIColor.systemBlue`
     public func radiusBorderColor(_ color: UIColor) -> LocationRadiusPickerConfigurationBuilder {
         configuration.radiusBorderColor = color
         return self
     }
     
-    /// Width of the radius circle border. Defaults to 3.0.
+    /// Width of the radius circle border.
     /// - Parameter width: width of the radius circle border
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `3.0`
     public func radiusBorderWidth(_ width: Double) -> LocationRadiusPickerConfigurationBuilder {
         configuration.radiusBorderWidth = width
         return self
     }
     
-    /// Fill color of the radius circle. Defaults to UIColor.systemBlue.withAlphaComponent(0.2).
+    /// Fill color of the radius circle.
     /// - Parameter color: Color of the radius circle
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `UIColor.systemBlue.withAlphaComponent(0.2)`
     public func radiusColor(_ color: UIColor) -> LocationRadiusPickerConfigurationBuilder {
         configuration.radiusColor = color
         return self
     }
     
-    /// Color of the radius label, which displays the current radius size. Defaults to UIColor.label.
+    /// Color of the radius label, which displays the current radius size.
     /// - Parameter color: text color of the radius label
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `UIColor.label`
     public func radiusLabelColor(_ color: UIColor) -> LocationRadiusPickerConfigurationBuilder {
         configuration.radiusLabelColor = color
         return self
     }
     
-    /// Color of the radius grabber (small circle with a pan gesture that resizes the radius). Defaults to UIColor.systemBlue.
+    /// Color of the radius grabber (small circle with a pan gesture that resizes the radius).
     /// - Parameter color: color of the radius grabber
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `UIColor.systemBlue`
     public func grabberColor(_ color: UIColor) -> LocationRadiusPickerConfigurationBuilder {
         configuration.grabberColor = color
         return self
     }
     
-    /// Size of the grabber view (small circle with a pan gesture that resizes the radius). Aspect ratio is 1:1. Defaults to 20pt.
+    /// Size of the grabber view (small circle with a pan gesture that resizes the radius). Aspect ratio is 1:1.
     /// - Parameter size: size of the grabber view
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `20.0`
     public func grabberSize(_ size: CGFloat) -> LocationRadiusPickerConfigurationBuilder {
         configuration.grabberSize = size
         return self
     }
     
-    /// Enables/disables the vibration effect when resizing the radius circle. Defaults to true.
+    /// Enables/disables the vibration effect when resizing the radius circle.
     /// - Parameter vibrate: enables vibration if set to true, otherwise disables it.
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `true`
     public func vibrateOnResize(_ vibrate: Bool) -> LocationRadiusPickerConfigurationBuilder {
         configuration.vibrateOnResize = vibrate
         return self
     }
     
-    /// Defines what unit system type should be used to displayed the length. Defaults to UnitSystemType.system.
+    /// Defines what unit system type should be used to displayed the length.
     /// - Parameter type: type of the unit system
     /// - Returns: updated builder
+    ///
+    /// - Note: Default value: `UnitSystemType.system`
     public func unitSystem(_ type: UnitSystemType) -> LocationRadiusPickerConfigurationBuilder {
         configuration.unitSystem = type
         return self
